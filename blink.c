f63fe89d3fd0ade7028a0a3b6a6e7f9359b6739d
Jason Woofenden <jason@jasonwoof.com>
2011-04-11 00:14:31 -0400

    factor out adc stuff, doesn't work

--- blink.c
+++ blink.c
@@ -23,7 +23,7 @@ show_10_bits(uint16_t bits) {
 	for(i = 0; i < 10; ++i) {
 		PORTB = 0xff;
 		delay(30);
-		if((bits & 0x20) == 0) {
+		if((bits & 0x200) == 0) {
 			PORTB = 0;
 		}
 		delay(150);
@@ -33,11 +33,39 @@ show_10_bits(uint16_t bits) {
 	}
 }
 
-#define read_channel 0
+void
+adc_init() {
+	ADMUX=(1<<REFS0); // voltage to Vcc which is hopefully 5v, but maybe requires me to set the reference voltage with wiring?
+	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); //ADEN=enable ADPS[123]=rescalar div factor 128 (frequency thing)
+
+	// some texts suggest a "dummy" read
+	ADCSRA |= (1<<ADSC); // activate the analog -> digital converter
+	while(!(ADCSRA &(1<<ADIF))) {
+		;
+	}
+	ADCSRA |= (1<<ADIF); // clear (yeah, I know, it's a weird way to clear) the ADIF bit for next time
+}
+
+uint16_t
+adc_read(channel) {
+	uint16_t l, h;
+
+	ADMUX = (ADMUX & 0xf8) | (channel & 0x7); // low 3 bits select read channel
+
+	ADCSRA |= (1<<ADSC); // activate the analog -> digital converter
+	while((ADCSRA & (1<<ADSC))) {
+		;
+	}
+	ADCSRA|=(1<<ADIF); // clear (yeah, I know, it's a weird way to clear) the ADIF bit for next time
+
+	l = ADCL; // book said to read the low first
+	h = ADCH;
+
+	return (h << 8) | l;
+}
  
 int
 main (void) {
-	uint16_t l, h;
 
 	// set all digital pins for output
 	DDRB = 0xff;
@@ -48,26 +76,11 @@ main (void) {
 	PORTD = 0xff;
 
 	// enable a/d conversion
-	ADMUX=(1<<REFS0); // voltage to Vcc which is hopefully 5v, but maybe requires me to set the reference voltage with wiring?
-	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); //ADEN=enable adps=Rrescalar div factor 128 (frequency)
-	while(!(ADCSRA * 0x10)) {
-		;
-	}
-	ADCSRA |= 0x10;
+	adc_init();
 
 	while (1) {
-		ADMUX |= (read_channel & 0x7); // low 3 bits select read channel
-
-		ADCSRA |= (1<<ADSC); // activate the analog -> digital converter
-		while(!(ADCSRA & (1<<ADIF))) {
-			;
-		}
-		ADCSRA|=(1<<ADIF); // clear (yeah, I know, it's a weird way to clear) the ADIF bit for next time
-		l = ADCL; // book said to read the low first
-		h = ADCH;
-		h = (h << 8) | l;
-
-		show_10_bits(h);
+		show_10_bits(ADSC);
+		//show_10_bits(adc_read(0));
 	}
 
 	return 1;
