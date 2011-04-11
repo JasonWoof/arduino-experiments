b61d24a75c8a44c9aa9ba5fd09a5b98300f4b3b7
Jason Woofenden <jason@jasonwoof.com>
2011-04-10 23:20:07 -0400

    analog read working at last

--- blink.c
+++ blink.c
@@ -2,69 +2,72 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void delay(uint8_t length) {
+	while (--length) {
+		_delay_loop_2(6000);
+	}
+}
+
 void
 delay_some() {
-	uint8_t counter = 0;
-	while (counter != 255) {
-		_delay_loop_2(3000);
-		counter++;
-	}
+	delay(255);
 }
 
 void
-show_16_bits(uint16_t bits) {
+show_10_bits(uint16_t bits) {
 	uint8_t i;
-	DDRD = 0x00;
+	PORTB = 0x00;
 	delay_some();
 	delay_some();
 	delay_some();
-	for(i = 0; i < 16; ++i) {
-		DDRD = 0x00;
-		delay_some();
-		// 0x04 is the yellow always pulser
-		// 0x08 is the red data pulser
-		DDRD = 0x04 | ((bits & 1) << 3); // move bit 
-		delay_some();
-		bits >>= 1;
+	for(i = 0; i < 10; ++i) {
+		PORTB = 0xff;
+		delay(30);
+		if((bits & 0x20) == 0) {
+			PORTB = 0;
+		}
+		delay(150);
+		PORTB = 0;
+		delay(250);
+		bits <<= 1;
 	}
 }
 
+#define read_channel 0
  
 int
 main (void) {
-	uint16_t counter;
-	uint16_t interval = 100;
+	uint16_t l, h;
 
-	/* set all digital pins for output*/
+	// set all digital pins for output
 	DDRB = 0xff;
 	DDRD = 0xff;
 
-	/* turn on pins 0-7 */
+	// turn all digital pins on
+	PORTD = 0xff;
 	PORTD = 0xff;
 
 	// enable a/d conversion
-	ADCSRA |= 0xc3;
-	
+	ADMUX=(1<<REFS0); // voltage to Vcc which is hopefully 5v, but maybe requires me to set the reference voltage with wiring?
+	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); //ADEN=enable adps=Rrescalar div factor 128 (frequency)
 	while(!(ADCSRA * 0x10)) {
 		;
 	}
 	ADCSRA |= 0x10;
 
 	while (1) {
-		/* set PORTB.2 high */
-		PORTB = 0x20;
-		// PORTD = 0x00;
+		ADMUX |= (read_channel & 0x7); // low 3 bits select read channel
 
-		ADMUX = 0x80; // pick the channel
-		ADCSRA |= (0x43); // activate the analog -> digital converter (can |3 here to scale down by 8x)
-		while(!(ADCSRA & 0x10)) {
+		ADCSRA |= (1<<ADSC); // activate the analog -> digital converter
+		while(!(ADCSRA & (1<<ADIF))) {
 			;
 		}
-		ADCSRA |= 0x10; // ?? reset the "done" bit (for next time?)
-		interval = ADCL; // book said to read the low first
-		interval |= ((uint16_t)ADCH << 8);
+		ADCSRA|=(1<<ADIF); // clear (yeah, I know, it's a weird way to clear) the ADIF bit for next time
+		l = ADCL; // book said to read the low first
+		h = ADCH;
+		h = (h << 8) | l;
 
-		show_16_bits(interval);
+		show_10_bits(h);
 	}
 
 	return 1;
